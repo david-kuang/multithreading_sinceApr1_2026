716090884a60942878ffceca63e4205a03b6a0c9
david-kuang <davidkuang416@gmail.com>
2026-04-02 07:39:41 -0400

    Update newfile_Apr1_2026.java

--- newfile_Apr1_2026.java
+++ newfile_Apr1_2026.java
@@ -1,1 +1,8 @@
-newfile_Apr1_2026
+//newfile_Apr1_2026 Thu Apr 2nd 2026
+1. Anthropic 什么是
+2. Claude Code v.1.88
+3. Someone use OpenAI Codex, convert it from JS/TS to Python, then RUST
+4. 什么是RUST
+Reference: https://github.com/instructkr/claw-code?tab=readme-ov-file 
+
+  

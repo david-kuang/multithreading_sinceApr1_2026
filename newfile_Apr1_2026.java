a40ac68f7dd4470035f2e31a63835034909bd529
davidkuang416@gmail.com <davidkuang416@gmail.com>
2026-04-02 23:51:59 -0400

    export TCP_DISCOVERY_IP_FINDER_PORT_RANGE=doublequote54570..54580doublequote

--- newfile_Apr1_2026.java
+++ newfile_Apr1_2026.java
@@ -5,4 +5,21 @@
 4. 什么是RUST
 Reference: https://github.com/instructkr/claw-code?tab=readme-ov-file 
 
-  
+.env 
+export TCP_DISCOVERY_IP_FINDER_PORT_RANGE, in mtb case, "54570..54580"
+To configure the port range for Apache Ignite 
+TCP_Discovery_IP_Finder, set the localPortRange property on the TcpDiscoverySpi bean in your Ignite configuration. The port ranger 
+determines how many ports, starting from localPort, the node will try to bind(捆绑/装订/约束/强迫/团结) to for discovery. 
+Setting Port Range in Configuration. You can define this in XML or Java:
+{
+TcpDiscoverySpi spi = new TcpDiscoverySpi();
+// Sets the local port to 47500 and checks 100 ports (47500-47599)
+spi.setLocalPort(47500);
+spi.setLocalPortRange(100); 
+
+IgniteConfiguration cfg = new IgniteConfiguration();
+cfg.setDiscoverySpi(spi);
+}
+
+Apache Ignite spi=serviceProviderInterface
+Sysplex, HOST_1_IP="$(host mvd1 | awk '{printf $5l exut}')"

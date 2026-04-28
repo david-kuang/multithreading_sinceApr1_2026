b4357bc4e70d2ab871896588620e952922569429
davidkuang416@gmail.com <davidkuang416@gmail.com>
2026-04-28 14:16:48 -0400

    To commit for fun

---
+++ rust.bat
@@ -0,0 +1,9 @@
+@echo off
+:: This is a batc file to check RUST version
+@echo on
+title TDEVYK3
+::echo RUST CARGO RUSTC
+::pause
+cargo --version
+rustc --version
+pause
---
+++ rustcargohw/target/debug/deps/rustcargohw.d
@@ -0,0 +1,5 @@
+C:\Users\David\github_com\multithreading_sinceApr1_2026\rustcargohw\target\debug\deps\rustcargohw.d: src\main.rs
+
+C:\Users\David\github_com\multithreading_sinceApr1_2026\rustcargohw\target\debug\deps\rustcargohw.exe: src\main.rs
+
+src\main.rs:

aae5e57d289e67ae5ba8b278c622476158cbe58d
davidkuang416@gmail.com <davidkuang416@gmail.com>
2026-04-30 18:31:19 -0400

    Init

---
+++ Gridgain_osapache_ibm/opensourceApacheIgnite/ignite-demov2_17/src/main/java/com/mtb/zdih/main/IgniteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mtb.zdih.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteDemoApplication.class, args);
+	}
+
+}

d8e52f2dfe381c5dbd244c53897706ce0ba41f10
davidkuang416@gmail.com <davidkuang416@gmail.com>
2026-04-30 19:01:57 -0400

    [18:59:13] Ignite node started OK (id=f6bda434)

--- Gridgain_osapache_ibm/opensourceApacheIgnite/ignite-demov2_17/src/main/java/com/mtb/zdih/main/IgniteDemoApplication.java
+++ Gridgain_osapache_ibm/opensourceApacheIgnite/ignite-demov2_17/src/main/java/com/mtb/zdih/main/IgniteDemoApplication.java
@@ -1,13 +1,23 @@
 package com.mtb.zdih.main;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.apache.ignite.Ignition;
 
 @SpringBootApplication
 public class IgniteDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IgniteDemoApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return args -> {
+			Ignition.start();
+		};
 	}
 
 }

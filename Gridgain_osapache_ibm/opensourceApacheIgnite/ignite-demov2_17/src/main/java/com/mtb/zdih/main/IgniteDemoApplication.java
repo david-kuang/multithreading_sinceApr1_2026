55d8001cd379af369e86f1f23b78be0a071a6f25
davidkuang416@gmail.com <davidkuang416@gmail.com>
2026-04-30 20:11:20 -0400

    ignite.cluster().state(ClusterState.ACTIVE) and DataRegionConfiguration drc.setPersistenceEnabled(true); Its permanent like for ever

--- Gridgain_osapache_ibm/opensourceApacheIgnite/ignite-demov2_17/src/main/java/com/mtb/zdih/main/IgniteDemoApplication.java
+++ Gridgain_osapache_ibm/opensourceApacheIgnite/ignite-demov2_17/src/main/java/com/mtb/zdih/main/IgniteDemoApplication.java
@@ -2,6 +2,11 @@ package com.mtb.zdih.main;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cluster.ClusterState;
+import org.apache.ignite.configuration.*;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,21 +15,54 @@ import org.apache.ignite.Ignition;
 
 @SpringBootApplication
 public class IgniteDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IgniteDemoApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(IgniteDemoApplication.class, args);
-	}
+    @Bean
+    public ApplicationRunner applicationRunner(Ignite ignite) {
+        return args -> {
+            IgniteCache<String, String> cache = ignite.getOrCreateCache("dummy");
+            //cache.put("key1", "value1");
+            System.out.println(cache.get("key1"));
+        };
+    }
 
-	@Bean
-	public ApplicationRunner applicationRunner() {
-		return args -> {
-			Ignite ignite = Ignition.start();
+    @Bean
+    public Ignite ignite() {
+        IgniteConfiguration cfg = new IgniteConfiguration();
+        cfg.setDataStorageConfiguration(getDataStorageConfiguration());
 
-			IgniteCache<String, String> cache = ignite.getOrCreateCache("dummy");
-			cache.put("key1","value1");
+        cfg.setCacheConfiguration(getCacheConfiguration());
 
-			System.out.println(cache.get("key1"));
-		};
-	}
+        Ignite ignite = Ignition.start(cfg);
+		ignite.cluster().state(ClusterState.ACTIVE);
 
+        return ignite;
+    }
+
+    private CacheConfiguration getCacheConfiguration() {
+        CacheConfiguration<String, String> cc = new CacheConfiguration<>();
+        cc.setName("dummy");
+        cc.setOnheapCacheEnabled(false);
+        cc.setBackups(1);
+        cc.setCacheMode(CacheMode.REPLICATED);
+        cc.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL);
+
+        return cc;
+    }
+
+    private static @NotNull DataStorageConfiguration getDataStorageConfiguration() {
+        DataRegionConfiguration drc = new DataRegionConfiguration();
+        drc.setName("my-data-region");
+        drc.setInitialSize(10 * 1024 * 1024);
+        drc.setMaxSize(40 * 1024 * 1024);
+        drc.setPageEvictionMode(DataPageEvictionMode.RANDOM_2_LRU);
+		drc.setPersistenceEnabled(true);
+
+        DataStorageConfiguration dsc = new DataStorageConfiguration();
+        dsc.setDefaultDataRegionConfiguration(drc);
+
+        return dsc;
+    }
 }

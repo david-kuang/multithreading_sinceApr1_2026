ed000a1318a2d8d23bbb66be348fec7917cf274c
davidkuang416@gmail.com <davidkuang416@gmail.com>
2026-05-01 03:31:07 -0400

    Looked like open-source Apache Ignite only v2.17 works

---
+++ Gridgain_osapache_ibm/opensourceApacheIgnite/ignite-demov2_18osApacheIgnite_onlyv2.17/src/main/java/com/mtb/zdih/main/IgniteDemoApplication.java
@@ -0,0 +1,69 @@
+package com.mtb.zdih.main;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cluster.ClusterState;
+import org.apache.ignite.configuration.*;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.apache.ignite.Ignition;
+
+@SpringBootApplication
+public class IgniteDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IgniteDemoApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(Ignite ignite) {
+        return args -> {
+            IgniteCache<String, String> cache = ignite.getOrCreateCache("dummy");
+            //cache.put("key1", "value1");
+            //cache.put("tdevyk3", "ykuang@mtb.com");
+            System.out.printf("cache.get(\"key1\") = %s, cache.get(\"tdevyk3\") = %s !", cache.get("key1"), cache.get("tdevyk3"));
+        };
+    }
+
+    @Bean
+    public Ignite ignite() {
+        IgniteConfiguration cfg = new IgniteConfiguration();
+        cfg.setDataStorageConfiguration(getDataStorageConfiguration());
+
+        cfg.setCacheConfiguration(getCacheConfiguration());
+
+        Ignite ignite = Ignition.start(cfg);
+		ignite.cluster().state(ClusterState.ACTIVE);
+
+        return ignite;
+    }
+
+    private CacheConfiguration getCacheConfiguration() {
+        CacheConfiguration<String, String> cc = new CacheConfiguration<>();
+        cc.setName("dummy");
+        cc.setOnheapCacheEnabled(false);
+        cc.setBackups(1);
+        cc.setCacheMode(CacheMode.REPLICATED);
+        cc.setAtomicityMode(CacheAtomicityMode.TRANSACTIONAL);
+
+        return cc;
+    }
+
+    private static @NotNull DataStorageConfiguration getDataStorageConfiguration() {
+        DataRegionConfiguration drc = new DataRegionConfiguration();
+        drc.setName("my-data-region");
+        drc.setInitialSize(10 * 1024 * 1024);
+        drc.setMaxSize(40 * 1024 * 1024);
+        drc.setPageEvictionMode(DataPageEvictionMode.RANDOM_2_LRU);
+		drc.setPersistenceEnabled(true);
+
+        DataStorageConfiguration dsc = new DataStorageConfiguration();
+        dsc.setDefaultDataRegionConfiguration(drc);
+
+        return dsc;
+    }
+}
---
+++ Gridgain_osapache_ibm/opensourceApacheIgnite/ignite-demov2_18osApacheIgnite_onlyv2.17/src/test/java/com/mtb/zdih/main/IgniteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtb.zdih.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteDemoApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}

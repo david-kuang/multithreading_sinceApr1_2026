7d22ff818e263070c02c8a1a484f28f425b72928
david-kuang <davidkuang416@gmail.com>
2026-05-11 10:51:24 -0400

    Add files via upload

---
+++ pythonFrameworks_WebDevelopmentFrameworks_DataSciences&AI_DataVisualization&UI/ftHW.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Hello, Tensorflow Version " + tf.__version__ + " !")
---
+++ pythonFrameworks_WebDevelopmentFrameworks_DataSciences&AI_DataVisualization&UI/kerasHelloworld.py
@@ -0,0 +1,6 @@
+import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Hello, Keras Version " + keras.__version__ + " !")
